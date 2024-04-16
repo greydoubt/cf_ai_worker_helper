cd2f8f48dd6b0421d8622446a1ab237bb539267d
pndr <ix@thepatient.org>
2024-04-15 20:18:34 -0700

    ensureType
    ensure type

--- ai.js
+++ ai.js
@@ -25,6 +25,60 @@ function ensureShape(shape, value) {
   }
 }
 
+
+
+
+function ensureType(type, value) {
+  if (isArray(value)) {
+    value.forEach(v => ensureType(type, v));
+    return;
+  }
+  switch (type) {
+    case 'bool' /* Bool */: {
+      if (typeof value === 'boolean') {
+        return;
+      }
+      break;
+    }
+    case 'float16' /* Float16 */:
+    case 'float32' /* Float32 */: {
+      if (typeof value === 'number') {
+        return;
+      }
+      break;
+    }
+    case 'int8' /* Int8 */:
+    case 'uint8' /* Uint8 */:
+    case 'int16' /* Int16 */:
+    case 'uint16' /* Uint16 */:
+    case 'int32' /* Int32 */:
+    case 'uint32' /* Uint32 */: {
+      if (Number.isInteger(value)) {
+        return;
+      }
+      break;
+    }
+    case 'int64' /* Int64 */:
+    case 'uint64' /* Uint64 */: {
+      if (typeof value === 'bigint') {
+        return;
+      }
+      break;
+    }
+    case 'str' /* String */: {
+      if (typeof value === 'string') {
+        return;
+      }
+      break;
+    }
+  }
+  throw new Error(`unexpected type "${type}" with value "${value}".`);
+}
+
+
+
+
+
 // check array lengths
 function arrLength(obj) {
   return obj instanceof TypedArrayProto
