b5c48bddc15f859eda7ae73426f7681b42478b40
greydoubt <43443470+greydoubt@users.noreply.github.com>
2024-04-15 20:11:06 -0700

    Create ai.js

---
+++ ai.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+// ../worker-constellation-entry/src/ai/tensor.ts
+var TypedArrayProto = Object.getPrototypeOf(Uint8Array);
+function isArray(value) {
+  return Array.isArray(value) || value instanceof TypedArrayProto;
+}
