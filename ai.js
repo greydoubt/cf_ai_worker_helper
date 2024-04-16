9b7288dc575a275beaa3881d236600675b256089
pndr <ix@thepatient.org>
2024-04-15 20:12:42 -0700

    Update ai.js

--- ai.js
+++ ai.js
@@ -4,3 +4,23 @@ var TypedArrayProto = Object.getPrototypeOf(Uint8Array);
 function isArray(value) {
   return Array.isArray(value) || value instanceof TypedArrayProto;
 }
+
+// base structures
+function ensureShape(shape, value) {
+  if (shape.length === 0 && !isArray(value)) {
+    return;
+  }
+  const count = shape.reduce((acc, v) => {
+    if (!Number.isInteger(v)) {
+      throw new Error(
+        `expected shape to be array-like of integers but found non-integer element "${v}"`
+      );
+    }
+    return acc * v;
+  }, 1);
+  if (count != arrLength(value)) {
+    throw new Error(
+      `invalid shape: expected ${count} elements for shape ${shape} but value array has length ${value.length}`
+    );
+  }
+}
